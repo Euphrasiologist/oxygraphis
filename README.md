6e692da96fc68930c0921528bf9db30f4d531130
Max Brown <mb39@sanger.ac.uk>
2022-08-21 16:19:43 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# `oxygraphis`
+
+A small crate and command line tool to interact with ecological graphs. The only graph type currently implemented is the bipartite graph.
+
+Algorithmic implementations to follow, but bipartite graphs can be visualised, as below.
+
+<img src="./euphrasia_hp.svg">
+
+*Oxygraphis* is one of only 5-6 genera in the flowering plants which have *graph* included fully in the name. It's in the Ranunculaceae.
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+// a binary to create a graph from
+// TSV input
+// and then we'd like to compute
+// various metrics on these graphs
+
+use clap::{arg, value_parser, Command};
+use oxygraph::BipartiteGraph;
+use std::error::Error;
+use std::path::PathBuf;
+
+fn cli() -> Command<'static> {
+    Command::new("oxygraphis")
+        .bin_name("oxygraphis")
+        .subcommand_required(false)
+        .arg_required_else_help(true)
+        .arg(
+            arg!(-i --input <FILE>)
+                // File always required
+                .required(true)
+                // and we expect it to be a PathBuf
+                .value_parser(value_parser!(PathBuf)),
+        )
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = cli().get_matches();
+
+    let input = matches.get_one::<PathBuf>("input").expect("required");
+
+    let bpgraph = BipartiteGraph::from_dsv(input, b'\t')?;
+
+    bpgraph.plot(420, 200);
+
+    Ok(())
+}
